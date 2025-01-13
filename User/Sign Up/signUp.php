9e08e6c77e14937c5ba7ce458faa5f561d4d0307
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2025-01-14 01:05:21 +0800

    signUp.php 0.01

--- User/Sign Up/signUp.php
+++ User/Sign Up/signUp.php
@@ -1,7 +1,12 @@
-<?php 
+<?php
 include('../../shared/connect.php');
 
-echo "Test";
+if (isset($_POST['btnRegister'])){
+    
+}
+
+
+$insertUserQuery = "INSERT INTO `tbl_users`(`email`, `contactNumber`, `password`) VALUES ('johndoe@gmail.com','09666933359','password1234')";
 ?>
 <!doctype html>
 <html lang="en">
@@ -27,71 +32,74 @@ echo "Test";
         <img src="../../assets/img/signup/bb wordmark sp.png" alt="Wordmark" class="img-fluid">
     </div>
 
-    <div class="container mt-2">
-        <div class="row justify-content-center g-1">
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-                <input type="text" class="form-control" placeholder="First Name">
+    <form method="POST">
+        <div class="container mt-2">
+            <div class="row justify-content-center g-1">
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <input type="text" class="form-control" placeholder="First Name">
+                </div>
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <input type="text" class="form-control" placeholder="Last Name">
+                </div>
             </div>
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-                <input type="text" class="form-control" placeholder="Last Name">
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Contact Number">
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <input type="text" class="form-control" placeholder="Lot no./Street/House no.">
+                </div>
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <select class="form-select customSelect form-select-md">
+                        <option selected>Select Province</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <select class="form-select customSelect form-select-md">
+                        <option selected>Select City</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                    <select class="form-select customSelect form-select-md">
+                        <option selected>Select Barangay</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
+                    <input type="password" class="form-control" placeholder="Password">
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-2">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
+                    <input type="password" class="form-control" placeholder="Confirm Password">
+                </div>
+            </div>
+            <div class="row justify-content-center g-1 mt-4">
+                <button type="submit" class="btn register-btn" name="btnRegister">Register</button>
             </div>
         </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-                <input type="text" class="form-control" placeholder="Contact Number">
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-                <input type="text" class="form-control" placeholder="Lot no./Street/House no.">
-            </div>
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-                <select class="form-select customSelect form-select-md">
-                    <option selected>Select Province</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-            <select class="form-select customSelect form-select-md">
-                    <option selected>Select City</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-            </select>
-            </div>
-            <div class="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2">
-            <select class="form-select customSelect form-select-md">
-                    <option selected>Select Barangay</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-            </select>
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-                <input type="text" class="form-control" placeholder="Email">
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-                <input type="password" class="form-control" placeholder="Password">
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-2">
-            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-                <input type="password" class="form-control" placeholder="Confirm Password">
-            </div>
-        </div>
-        <div class="row justify-content-center g-1 mt-4">
-            <button type="button" class="btn register-btn">Register</button>
-        </div>
-    </div>
-    </div>
+    </form>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN6jIeHz"
