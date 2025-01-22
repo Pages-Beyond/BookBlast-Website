9fb8345d8bbac7552de1b7e60c2f1ba42debf4ba
Justine <justineraymundo18@gmail.com>
2025-01-22 18:44:43 +0800

    slight changes

--- Admin/adminProfile.php
+++ Admin/adminProfile.php
@@ -8,8 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="assets/img/bookblast-logo.png" />
-    <link rel="stylesheet" href="assets/css/adminProfile.css">
+    <link rel="icon" href="../assets/admin/img/bookblast-logo.png" type="image/png">
+    <link rel="stylesheet" href="../assets/admin/css/adminProfile.css">
 
 </head>
 
@@ -49,7 +49,7 @@
             <!-- Profile Photo on the Left -->
             <div class="col-12 col-md-4 text-center mb-4 mb-md-0">
                 <div class="profile-photo-container">
-                    <img id="profile-photo" src="assets/img/artN.jpg" alt="Profile Photo"
+                    <img id="profile-photo" src="../assets/admin/img/artN.jpg" alt="Profile Photo"
                         class="rounded-circle" style="width: 200px; height: 200px; object-fit: cover; margin-right: 50px;">
                 </div>
 
--- Admin/requests.php
+++ Admin/requests.php
@@ -1,3 +1,18 @@
+<?php
+include("connect.php");
+
+$userInfoQuery = "SELECT * FROM tbl_users";
+$userInfoResult = executeQuery($userInfoQuery);
+
+$booksInfoQuery = "SELECT DISTINCT(bookTitle), tbl_books.bookID, tbl_books.categoryID, tbl_books.bookCover, tbl_authors.* FROM tbl_books 
+                INNER JOIN tbl_authors ON tbl_books.authorID = tbl_authors.authorID";
+$booksInfoResult = executeQuery($booksInfoQuery);
+
+if (isset($_POST['btnAccept'])) {
+
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -8,17 +23,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link rel="icon" href="assets/img/bookblast-logo.png" type="image/png">
-    <link rel="stylesheet" href="assets/css/requests.css">
+    <link rel="icon" href="../assets/admin/img/bookblast-logo.png" type="image/png">
+    <link rel="stylesheet" href="../assets/admin/css/requests.css">
 
 </head>
 
 <body>
     <!-- Navbar -->
-    <?php include("assets/shared/navbar.php"); ?>
+    <?php include("../assets/admin/shared/navbar.php"); ?>
 
     <!-- Sidebar -->
-    <?php include("assets/shared/sidebar.php"); ?>
+    <?php include("../assets/admin/shared/sidebar.php"); ?>
 
     <!-- Main Content -->
     <div class="main-content col-10 d-flex flex-column align-items-start">
@@ -26,79 +41,53 @@
             <h1>Requests</h1>
         </div>
 
-        <!-- Card Row -->
-        <div class="row ms-1 g-5">
-            <!-- First Card -->
-            <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                <div class="card" style="max-width: 16rem; min-width: 12rem;">
-                    <img src="assets/img/peterpan.png" class="card-img-top" alt="Dashboard Image"
-                        style="height: 160px; object-fit: cover;">
-                </div>
-                <div class="mt-2">
-                    <h2 class="mb-0" style="white-space: nowrap;">Peter Pan</h2>
-                    <h6 class="mb-0" style="white-space: nowrap;">John Doe</h6>
-                </div>
-            </div>
 
-            <!-- Right Aligned Name, Email, Phone, and Buttons-->
-            <div class="col-md-4 col-lg-4 d-flex flex-column align-items-start justify-content-center mt-0 order-md-1 order-2 ms-md-auto"
-                style="margin-right: 20px;">
-                <div class="d-flex flex-column align-items-start">
-                    <h1 class="mb-0" style="white-space: nowrap;">John Doe</h1>
-                    <h6 class="mb-1">johndoe@gmail.com</h6>
-                    <h6 class="mb-0">097624168632</h6>
-                </div>
+        <?php
+        if (mysqli_num_rows($booksInfoResult) > 0) {
+            while ($bookInfoRows = mysqli_fetch_assoc($booksInfoResult)) { ?>
 
-                <!-- Buttons below phone number -->
-                <div class="mt-4 d-flex justify-content-between" style="gap: 70px;">
-                    <div class="col-6 col-sm-12">
-                        <button class="btn w-100"
-                            style="background-color: #7D97A0; border-radius: 30px; color: white;">Allow</button>
+                <!-- Card Row -->
+                <div class="row ms-1 g-5">
+                    <!-- Transaction Creation -->
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-12">
+                        <div class="card" style="max-width: 16rem; min-width: 12rem;">
+                            <img src="../assets/shared/img/bookCovers/<?php echo $bookInfoRows['bookCover'] ?>"
+                                class="card-img-top" alt="Dashboard Image" style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="mb-5">
+                            <h2 class="mb-0" style="white-space: nowrap;"><?php echo $bookInfoRows['bookTitle'] ?></h2>
+                            <h6 class="mb-0" style="white-space: nowrap;">
+                                <?php echo $bookInfoRows['firstName'] . " " . $bookInfoRows['lastName'] ?></h6>
+                        </div>
                     </div>
-                    <div class="col-6 col-sm-12">
-                        <button class="btn w-100"
-                            style="background-color: #B26424; border-radius: 30px; color: white;">Decline</button>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <!-- Second Card Row -->
-        <div class="row mt-2 ms-1 g-5">
-            <!-- Second Card -->
-            <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                <div class="card" style="max-width: 16rem; min-width: 12rem;">
-                    <img src="assets/img/peterpan.png" class="card-img-top" alt="Dashboard Image"
-                        style="height: 160px; object-fit: cover;">
-                </div>
-                <div class="mt-2">
-                    <h2 class="mb-0" style="white-space: nowrap;">Peter Pan</h2>
-                    <h6 class="mb-0" style="white-space: nowrap;">John Doe</h6>
-                </div>
-            </div>
+                        <div class="col-md-4 col-lg-4 d-flex flex-column align-items-start justify-content-center mt-0 order-md-1 order-2 ms-md-auto"
+                            style="margin-right: 20px;">
+                            <div class="d-flex flex-column align-items-start">
+                                <h1 class="mb-0" style="white-space: nowrap;">John Doe</h1>
+                                <h6 class="mb-1">johndoe@gmail.com</h6>
+                                <h6 class="mb-0">097624168632</h6>
+                            </div>
 
-            <!-- Right Aligned Name, Email, Phone, and Buttons -->
-            <div class="col-md-4 col-lg-4 d-flex flex-column align-items-start justify-content-center mt-0 order-md-1 order-2 ms-md-auto"
-                style="margin-right: 20px;">
-                <div class="d-flex flex-column align-items-start">
-                    <h1 class="mb-0" style="white-space: nowrap;">John Doe</h1>
-                    <h6 class="mb-1">johndoe@gmail.com</h6>
-                    <h6 class="mb-0">097624168632</h6>
-                </div>
-
-                <!-- Buttons below phone number -->
-                <div class="mt-4 d-flex justify-content-between " style="gap: 70px;">
-                    <div class="col-6 col-sm-12">
-                        <button class="btn w-100"
-                            style="background-color: #7D97A0; border-radius: 30px; color: white;">Allow</button>
-                    </div>
-                    <div class="col-6 col-sm-12">
-                        <button class="btn w-100"
-                            style="background-color: #B26424; border-radius: 30px; color: white;">Decline</button>
+                        <form method="POST">
+                            <div class="mt-4 d-flex justify-content-between" style="gap: 70px;">
+                                <div class="col-6 col-sm-12">
+                                    <button class="btn w-100"
+                                        style="background-color: #7D97A0; border-radius: 30px; color: white;">Allow</button>
+                                </div>
+                                <div class="col-6 col-sm-12">
+                                    <button class="btn w-100"
+                                        style="background-color: #B26424; border-radius: 30px; color: white;">Decline</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
-        </div>
+
+                <?php
+            }
+        }
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
         <script>
