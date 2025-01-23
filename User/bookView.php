866d74bc1e2084d9a6ac138001a3fe2ffb0797f5
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2025-01-23 23:52:12 +0800

    BookView Revisions

--- User/bookView.php
+++ User/bookView.php
@@ -4,44 +4,78 @@ session_start();
 
 
 $userID = $_SESSION['userID'];
+
 $bookTitle = '';
 $bookAuthor = '';
 $bookCover = '';
 $bookRating = '';
+$bookID = '';
+$dateReturned ='';
+$disabled = '';
 
-if (isset($_GET['bookID'])){
+if (isset($_GET['bookID']) && $_GET['bookID'] != '') {
     $bookID = $_GET['bookID'];
+
+} else {
+    header('location: ../');
+
+}
+
+if ($bookID != '') {
     $getBookDataQuery = "SELECT * from tbl_Books 
     LEFT JOIN tbl_authors ON tbl_books.authorID = tbl_authors.authorID 
     WHERE tbl_books.bookID = '$bookID';";
 
     $getBookRatingQuery = "SELECT ROUND(AVG(userRating),1) as bookRating from tbl_reviews WHERE bookID = '$bookID';";
-    
+
     $bookDataResult = executeQuery($getBookDataQuery);
 
-    while ($bookDataRow = mysqli_fetch_assoc($bookDataResult)){
+    while ($bookDataRow = mysqli_fetch_assoc($bookDataResult)) {
         $bookTitle = $bookDataRow['bookTitle'];
-        $bookAuthor = $bookDataRow['firstName']." ". $bookDataRow['lastName'];
+        $bookAuthor = $bookDataRow['firstName'] . " " . $bookDataRow['lastName'];
         $bookCover = $bookDataRow['bookCover'];
-      
+
     }
 
     $bookRatingResult = executeQuery($getBookRatingQuery);
-    while ($bookRatingRow = mysqli_fetch_assoc($bookRatingResult)){
+    while ($bookRatingRow = mysqli_fetch_assoc($bookRatingResult)) {
         $bookRating = $bookRatingRow['bookRating'];
     }
 
 
 
+}
+
+if (isset($_POST['btnBorrow'])) {
+    $insertUserTransactionQuery = "INSERT INTO `tbl_transactions`(`bookID`, `userID`, `isApproved`, `isDeclined`, `status`) 
+    VALUES ('$bookID','$userID','pending','no','pending',);";
+
+    executeQuery($insertUserTransactionQuery);
 
 
-}else{
-    header('Location: ../');
 }
 
 
 
+
+
+$getUserTransactionQuery ="SELECT tbl_transactions.dateReturned, tbl_books.stocks, tbl_users.userLimit FROM `tbl_transactions` LEFT JOIN tbl_users ON tbl_transactions.userID = tbl_users.userID LEFT JOIN tbl_books ON tbl_transactions.bookID = tbl_books.bookID WHERE tbl_transactions.userID = '$userID' AND tbl_transactions.bookID='$bookID';";
+$userTransactionResult = executeQuery($getUserTransactionQuery);
+
+while ($row = mysqli_fetch_assoc($userTransactionResult)){
+    $dateReturned = $row['dateReturned'];
+    $stocks = $row['stocks'];
+    $userLimit = $row['userLimit'];
+    $disabled = ($dateReturned == NULL || $stocks <= 0 || $userLimit == 5) ? 'disabled': '';
+
+}
+
+
+
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -162,7 +196,7 @@ if (isset($_GET['bookID'])){
         <div class="mb-4">
             <!-- Title Section with Back Arrow -->
             <div class="title-container">
-                <a href="#" onclick="history.go(-1)">
+                <a href="../">
                     <div class="back-arrow">
                         <i class="fa fa-chevron-left text-white" style="font-size: 2rem;"></i>
                     </div>
@@ -170,10 +204,10 @@ if (isset($_GET['bookID'])){
 
                 <div class="title-content">
                     <h2 class="text-white m-0" style="font-size: 4rem;">
-                        <?php echo $bookTitle ?>
+                    <?php echo $bookTitle?>
                     </h2>
                     <div class="icons-wrapper">
-                        <input type="checkbox" class="heart-checkbox" id="heart-checkbox">
+                        <input type="checkbox" class="heart-checkbox" id="heart-checkbox" name="favoriteBtn">
                         <label for="heart-checkbox" class="heart">❤</label>
                         <input type="checkbox" class="wishlist-checkbox" id="wishlist-checkbox">
                         <label for="wishlist-checkbox" class="wishlist">
@@ -189,10 +223,10 @@ if (isset($_GET['bookID'])){
 
             <!-- Author and Rating -->
             <div class="author-section mt-2">
-                <h3 class="text-white mb-1" style="font-size: 2rem;"><?php echo $bookAuthor?></h3>
+                <h3 class="text-white mb-1" style="font-size: 2rem;"><?php echo $bookAuthor ?></h3>
                 <div class="d-flex align-items-center gap-2">
                     <span class="fa fa-star checked"></span>
-                    <span class="text-white"><?php echo $bookRating?>/5</span>
+                    <span class="text-white"><?php echo $bookRating ?>/5</span>
                 </div>
             </div>
         </div>
@@ -202,70 +236,94 @@ if (isset($_GET['bookID'])){
         <div class="row justify-content-center mb-5">
             <div class="col-12 col-sm-10 col-md-8 col-lg-6">
                 <div class="card" style="border-radius: 10px;">
-                    <img src="../assets/shared/img/bookCovers/<?php echo $bookCover?>" class="card-img-top img-fluid" alt="Book Image"
-                        style="max-width: 100%; height: auto;">
+                    <img src="../assets/shared/img/bookCovers/<?php echo $bookCover ?>" class="card-img-top img-fluid"
+                        alt="Book Image" style="max-width: 100%; height: auto;">
                 </div>
 
                 <div class="d-flex justify-content-end mt-3">
-                    <button class="btn"
-                        style="background-color: #7D97A0; color: white; padding: 8px 14px; font-size: 16px;">Borrow</button>
+              
+
+                    <button type='button' class="btn" data-bs-toggle="modal"
+                        style="background-color: #7D97A0; color: white; padding: 8px 14px; font-size: 16px"
+                        data-bs-target="#deleteModal"<?php echo $disabled?>>Borrow</button>
+                    <form method="POST" action="bookView.php?bookID=<?php echo $bookID ?>">
+                        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">BookBlast:
+                                            <?php echo $bookTitle ?></h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        Do you want to borrow this book?
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="submit" name="btnBorrow" class="btn btn-primary"
+                                            >Yes</button>
+                                        <button type="button" class="btn btn-danger"
+                                            data-bs-dismiss="modal">Cancel</button>
+                                    </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
+
+    </div>
+    </div>
 
 
-        <!-- Reviews Section -->
-        <h2 class="text-white mb-4">Reviews</h2>
-        <div class="row g-4">
-            <?php 
-            $getUserReviewQuery = "SELECT tbl_reviews.userID, tbl_reviews.userReview, tbl_reviews.userRating, tbl_users.userProfilePic, tbl_userinfo.firstName, tbl_userinfo.firstName FROM `tbl_reviews` 
+    <!-- Reviews Section -->
+    <h2 class="text-white mb-4">Reviews</h2>
+    <div class="row g-4">
+        <?php
+        $getUserReviewQuery = "SELECT tbl_reviews.userID, tbl_reviews.userReview, tbl_reviews.userRating, tbl_users.userProfilePic, tbl_userinfo.firstName, tbl_userinfo.lastName FROM `tbl_reviews` 
             LEFT JOIN tbl_users ON tbl_reviews.userID = tbl_users.userID
             LEFT JOIN tbl_userinfo ON tbl_users.userID = tbl_userinfo.userID 
             WHERE bookID = $bookID;";
-            $userReviewResult = executeQuery($getUserReviewQuery);
-            while ($userReviewRows = mysqli_fetch_assoc($userReviewResult) ){
-                $userReview = $userReviewRows['userReview'];
-                $userRating = $userReviewRows['userRating'];
-                $userProfilePic = $userReviewRows['userProfilePic'];
-                
+        $userReviewResult = executeQuery($getUserReviewQuery);
+        while ($userReviewRows = mysqli_fetch_assoc($userReviewResult)) {
+            $userReview = $userReviewRows['userReview'];
+            $userRating = $userReviewRows['userRating'];
+            $userProfilePic = $userReviewRows['userProfilePic'];
+            $userName = $userReviewRows['firstName'] . " " . $userReviewRows['lastName'];
 
-          
+
+
             ?>
             <div class="col-12 col-lg-6">
                 <div class="card">
                     <div class="d-flex p-3">
-                        <img src="../assets/shared/img/userpfp/<?php echo $userProfilePic?>" class="rounded-circle"
+                        <img src="../assets/shared/img/userpfp/<?php echo $userProfilePic ?>" class="rounded-circle"
                             style="width: 80px; height: 80px;" alt="Profile">
                         <div class="ms-3">
-                            <h5 class="card-title">Janna Macatangay</h5>
-                            <p class="card-text"><?php echo $userReview?></p>
+                            <h5 class="card-title"><?php echo $userName ?></h5>
+                            <p class="card-text"><?php echo $userReview ?></p>
                             <div>
+                                <span><?php echo $userRating ?>/5</span>
                                 <?php
-                               
-                                for ($i=0; $i<5; $i++){
-                                    for ($j=0; $j<4; $j++){
-                                        $checked = 'checked';
-                                        if ($j == 3) {
-                                            $checked =' ';
-                                        }
-                                    }
-                              
+                                for ($i = 0; $i < 5; $i++) {
+                                    $checked = ($i < $userRating) ? 'checked' : '';
                                     ?>
-                                  
-                                 <span class="fa fa-star <?php echo $checked?>" ></span>
-                                <?php
-                                };
-                                ?> 
+                                    <span class="fa fa-star <?php echo $checked; ?>"></span>
+                                    <?php
+                                }
+                                ?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <?php 
-            }
-            ?>
-          
-        </div>
+            <?php
+        }
+        ?>
+
+    </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
